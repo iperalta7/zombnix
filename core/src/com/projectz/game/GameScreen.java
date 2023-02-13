8adda6f3e7762e9e45dd8b3cfe46d505158e0f5a
Aaron <abun02@uri.edu>
2023-02-13 12:24:20 -0500

    main menu stuff

---
+++ core/src/com/projectz/game/GameScreen.java
@@ -0,0 +1,31 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.projectz.game.ProjectZ;
+
+public class GameScreen extends ScreenAdapter {
+    ProjectZ game;
+
+    public GameScreen(final ProjectZ game) {
+        this.game = game;
+    }
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, .25f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+}
