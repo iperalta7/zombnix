ec92702ee5ca92596b959a935e80f59ab684e230
Aaron <abun02@uri.edu>
2023-02-13 12:43:27 -0500

    recommit for main menu

--- core/src/com/projectz/game/ProjectZ.java
+++ core/src/com/projectz/game/ProjectZ.java
@@ -1,31 +1,34 @@
 package com.projectz.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class ProjectZ extends ApplicationAdapter {
+public class ProjectZ extends Game {
 	SpriteBatch batch;
 	Texture img;
+	ShapeRenderer shapeRenderer;
+	BitmapFont font;
+
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		shapeRenderer = new ShapeRenderer();
+		font = new BitmapFont();
+		this.setScreen(new MainMenu(this));
+
 	}
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
 	@Override
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+		shapeRenderer.dispose();
+		font.dispose();
 	}
 }
