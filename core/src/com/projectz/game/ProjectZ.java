b04a37f626b37fd6c1d0579b0c990b61133dd335
Isaiah Peralta <iperalta@uri.edu>
2023-02-13 17:53:56 -0500

    add logic to player movement w/ basic stage setup in game
    player inherits from Actor which will allow the player to be drawn onto a stage -the player is able to move up/down/left/right (no animations)

--- core/src/com/projectz/game/ProjectZ.java
+++ core/src/com/projectz/game/ProjectZ.java
@@ -3,9 +3,12 @@ package com.projectz.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.projectz.game.player.Player;
 import com.projectz.game.utils.MyInputProcessor;
 
 import com.badlogic.gdx.Input.Keys;
@@ -16,25 +19,29 @@ public class ProjectZ extends ApplicationAdapter {
 
 	MyInputProcessor inputProcessor = new MyInputProcessor();
 
+	private Player player;
+	private Texture playerTexture;
+	Stage stage;
 
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create() {
+		stage = new Stage();
+		player = new Player();
+		stage.addActor(player);
 		Gdx.input.setInputProcessor(inputProcessor);
 	}
 
 	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void render() {
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		stage.act(Gdx.graphics.getDeltaTime());
+		stage.draw();
 	}
-	
+
 	@Override
-	public void dispose () {
+	public void dispose() {
 		batch.dispose();
 		img.dispose();
+		playerTexture.dispose();
 	}
 }
--- core/src/com/projectz/game/player/Player.java
+++ core/src/com/projectz/game/player/Player.java
@@ -1,23 +1,53 @@
-package com.projectz.game;
+package com.projectz.game.player;
 
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.graphics.Texture;
+
 
 //Player.java
 public class Player extends Actor {
-    private SpriteBatch spriteBatch;
-    private ShapeRenderer shapeRenderer;
+    private Vector2 position;
+    private float speed;
+    private Texture playerTexture;
 
-    public Player(float positionX, float positionY) {
-        this.setPosition(positionX, positionY);
-        spriteBatch = new SpriteBatch();
-        shapeRenderer = new ShapeRenderer();
+    public Player () {
+        position = new Vector2();
+        speed = 30f;
+        playerTexture = new Texture("player.png");
     }
 
-    public enum Movement {
+    @Override
+    public void act(float deltaTime) {
+        super.act(deltaTime);
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+            position.y += speed * deltaTime;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            position.x -= speed * deltaTime;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+            position.y -= speed * deltaTime;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+            position.x += speed * deltaTime;
+        }
+    }
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
+        // Draw the player sprite at the current position
+        batch.draw(playerTexture, position.x, position.y);
 
     }
 
+    public void dispose() {
+        playerTexture.dispose();
+    }
 }
+
+
+
