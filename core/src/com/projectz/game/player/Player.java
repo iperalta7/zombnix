c7b9afc3fbd295dc88cc357120b2e6a3266572ef
Isaiah Peralta <iperalta@uri.edu>
2023-02-14 10:16:31 -0500

    implemented an orthographic camera attached to the player

--- core/src/com/projectz/game/player/Player.java
+++ core/src/com/projectz/game/player/Player.java
@@ -6,10 +6,13 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 
 
 //Player.java
 public class Player extends Actor {
+
+    private OrthographicCamera camera;
     private Vector2 position;
     private float speed;
     private Texture playerTexture;
@@ -22,6 +25,8 @@ public class Player extends Actor {
         position = new Vector2();
         speed = 30f;
         playerTexture = new Texture("player.png");
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false,Gdx.graphics.getWidth(), Gdx.graphics.getHeight() );
     }
 
     //changes the position of player object based on input
@@ -40,12 +45,22 @@ public class Player extends Actor {
         if (Gdx.input.isKeyPressed(Input.Keys.D)) {
             position.x += speed * deltaTime;
         }
+
+        // update the camera position to follow the player
+        camera.position.x = position.x;
+        camera.position.y = position.y;
+        camera.update();
     }
 
     //draw method for player
     @Override
     public void draw(Batch batch, float parentAlpha) {
         super.draw(batch, parentAlpha);
+
+        // set the Batch projection matrix to match the camera's view
+        batch.setProjectionMatrix(camera.combined);
+
+
         // Draw the player sprite at the current position
         batch.draw(playerTexture, position.x, position.y);
 
