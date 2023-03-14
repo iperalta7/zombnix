9de7f50212264a3536c76e9e2e16434c2dc19cce
Demetrios Petrou <demetripetrou1@gmail.com>
2023-03-14 01:07:40 -0400

    Made abstract enemy an actor that can be added to the same stage as the player for easy rendering and update.

--- core/src/com/projectz/game/enemies/Enemy.java
+++ core/src/com/projectz/game/enemies/Enemy.java
@@ -2,21 +2,45 @@ package com.projectz.game.enemies;
 
 import java.util.Random;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.projectz.game.items.Item;
 
-public abstract class Enemy {
+
+public abstract class Enemy extends Actor {
     
     Item[] drops;
     int health;
+    protected Texture enemyTexture;
+    protected Vector2 position;
+    protected float speed;
     
     public Enemy(Item[] dropItems, int health){
         this.drops = dropItems;
         this.health = health;
+        this.position = new Vector2();
+        this.speed = 50f;
     }
-    
-    public abstract void Update();
-    public abstract void Render();
-    public abstract void attack();
+
+    public Vector2 getPosition(){
+        return position;
+    }
+
+    public abstract void act(float deltaTime);
+
+    public void draw(Batch batch, float parentAlpha) {
+        // Draw the player sprite at the current position
+        batch.draw(enemyTexture, position.x, position.y);
+    }
+
+    //used for memory management
+    public void dispose() {
+        enemyTexture.dispose();
+    }
     
     public void onDeath(){
         // Random r = new Random();
--- core/src/com/projectz/game/enemies/EnemyBoss.java
+++ core/src/com/projectz/game/enemies/EnemyBoss.java
@@ -1,25 +1,43 @@
 package com.projectz.game.enemies;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.projectz.game.items.Item;
+import com.badlogic.gdx.math.Vector2;
+
+
 
 public class EnemyBoss extends Enemy{
-
     public EnemyBoss(){
         super(new Item[] {Item.HealingPotion}, 100);
+
+        Pixmap pixmap200 = new Pixmap(Gdx.files.internal("C:/Users/demet/Documents/URI_Folders/CSC305/s23g/assets/Zombie/zombie_man.png"));
+
+        Pixmap pixmap100 = new Pixmap(50, 50, pixmap200.getFormat());
+        pixmap100.drawPixmap(pixmap200,
+                0, 0, pixmap200.getWidth(), pixmap200.getHeight(),
+                0, 0, pixmap100.getWidth(), pixmap100.getHeight()
+        );
+        enemyTexture = new Texture(pixmap100);
+        pixmap200.dispose();
+        pixmap100.dispose();
     }
 
-    @Override
-    public void Update() {
+    public void act(float deltaTime){
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            position.y += speed * deltaTime;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            position.x -= speed * deltaTime;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            position.y -= speed * deltaTime;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            position.x += speed * deltaTime;
+        }
     }
-
-    @Override
-    public void Render() {
-
-    }
-
-    @Override
-    public void attack(){
-
-    }
-
 }
