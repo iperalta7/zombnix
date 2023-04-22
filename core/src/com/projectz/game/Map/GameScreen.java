c860a925e0138f34b5517e3dd4a14c9f8ec14762
Isaiah Peralta <iperalta@uri.edu>
2023-04-22 16:59:16 -0400

    ENHANCEMENT: refactored the GameScreen code for better readability and debugging

--- core/src/com/projectz/game/Map/GameScreen.java
+++ core/src/com/projectz/game/Map/GameScreen.java
@@ -1,26 +1,16 @@
 package com.projectz.game.Map;
 
 
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.Gdx; 
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-
 import com.badlogic.gdx.*;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-
-import com.projectz.game.player.Player;
-import com.projectz.game.enemies.EnemyGeneric;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.projectz.game.ProjectZ;
+import com.projectz.game.enemies.EnemyGeneric;
 import com.projectz.game.inventory.Inventory;
 import com.projectz.game.items.Item;
 import com.projectz.game.player.Player;
@@ -29,7 +19,6 @@ import com.projectz.game.ui.HotBar;
 import com.projectz.game.ui.HotBarRenderer;
 import com.projectz.game.ui.StatusHUD;
 import com.projectz.game.ui.StatusHUDRenderer;
-
 
 public class GameScreen implements Screen{
     private TiledMap map;
@@ -54,68 +43,108 @@ public class GameScreen implements Screen{
 
     @Override
     public void render(float delta){
-        Gdx.gl.glClearColor(0,0,0,1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-
-        // update the camera position to follow the player
-        camera.position.x = player.getPosition().x;
-        camera.position.y = player.getPosition().y;
-        camera.update();
-
+        clearScreen();
+        updateCamera();
         renderer.setView(camera);
         renderer.render();
+        handleInput();
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
+    }
 
+    /**
+     * Clears the screen with a black background.
+     */
+    private void clearScreen() {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    private void handleInput() {
         Gdx.input.setInputProcessor(new InputAdapter() {
             @Override
-            public boolean keyDown (int keyCode) {
+            public boolean keyDown(int keyCode) {
                 if (keyCode == Input.Keys.E) {
                     game.setScreen(new InventoryScreen((ProjectZ) game, inventory));
                 }
                 return true;
             }
         });
+    }
 
-        //default call to create stage (from documentation page)
-        stage.act(Gdx.graphics.getDeltaTime());
-        stage.draw();
-
+    /**
+     * Updates the camera's position to follow the player.
+     */
+    private void updateCamera() {
+        camera.position.x = player.getPosition().x;
+        camera.position.y = player.getPosition().y;
+        camera.update();
     }
 
     @Override
     public void resize(int width, int height){
         camera.viewportWidth = width; 
         camera.viewportHeight = height;
-
         camera.position.set(camera.viewportWidth / 3f, camera.viewportHeight / 3f, 0);
-        camera.update(); 
-
+        camera.update();
     }
-
 
     @Override
     public void show(){
+        setupGame();
+    }
+
+    /**
+     * Sets up the game components, such as the map, player, enemy, and user interface.
+     */
+    private void setupGame() {
         map = new TmxMapLoader().load("maps/zombie_map.tmx");
         renderer = new OrthogonalTiledMapRenderer(map, 3f);
-        player = new Player();
-        player.setPlayerPosition(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
-        enemy = new EnemyGeneric(null, 100, player);
+        setupCamera();
+        setupPlayer();
+        setupEnemy();
+        setupUI();
+    }
+
+    /**
+     * Sets up the camera with the appropriate settings.
+     */
+    private void setupCamera() {
         camera = new OrthographicCamera();
-        camera.setToOrtho(false,Gdx.graphics.getWidth(), Gdx.graphics.getHeight() );
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+    }
+
+    /**
+     * Sets up the player and its initial position.
+     */
+    private void setupPlayer() {
+        player = new Player();
+        player.setPlayerPosition(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
+    }
+
+    /**
+     * Sets up the enemy with the appropriate attributes.
+     */
+
+    private void setupEnemy() {
+        enemy = new EnemyGeneric(null, 100, player);
+    }
+
+    /**
+     * Sets up the user interface components, such as the stage, status HUD, hotbar, and inventory.
+     */
+    private void setupUI() {
         statusHUDRenderer = new StatusHUDRenderer(new StatusHUD(player), player);
         stage = new Stage();
         inventory = new Inventory();
         inventory.addItem(Item.HealingPotion, 5);
         hotBar = new HotBar(inventory);
         hotBarRenderer = new HotBarRenderer(hotBar);
-        //inventory.addItem(Item.SpeedPotion, 5);
         stage.addActor(player);
         stage.addActor(statusHUDRenderer);
         stage.addActor(hotBarRenderer);
         stage.addActor(enemy);
-
-    }   
-
+    }
 
     @Override
     public void hide(){
